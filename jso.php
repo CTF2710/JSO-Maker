0d75b413a6af29103a4a7ebdb29d4dc51669ad5c
CTF2710 <71294688+CTF2710@users.noreply.github.com>
2020-09-22 12:17:15 +0700

    Update jso.php

--- jso.php
+++ jso.php
@@ -1,6 +1,1 @@
-    <h1>Jso Converter<br>IFC-Team</h1>
-    <form name="charCodeAt" method="post">
-        <textarea name="input" placeholder="Script Deface"></textarea><br><br>
-        <input type="button" onclick="runCharCodeAt()" value="Convert"><br><br>
-        <textarea name="output" placeholder="Hasil Convert"></textarea><br><br>
-        <input type="submit" name="submit" value="Pastebin
+WTF
